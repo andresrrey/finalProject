3fc79684e55582e789939eda6397428baf510129
Andres Romero <andres.romero@endava.com>
2017-11-24 10:15:57 -0500

    Adding topic data to interface

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,21 +9,19 @@ class TeamsController < ApplicationController
     # Messages will not be marked as processed automatically. If you shut down the
     # consumer without calling `#mark_message_as_processed` first, the consumer will
     # not resume where you left off!
-    consumer.each_message(automatically_mark_as_processed: false) do |message|
-      # Our messages are JSON with a `type` field and other stuff.
-      consumer.subscribe("CTF_countriesbyteam")
+    # Our messages are JSON with a `type` field and other stuff.
+    consumer.subscribe("CTF_countriesbyteam")
 
-      # Stop the consumer when the SIGTERM signal is sent to the process.
-      # It's better to shut down gracefully than to kill the process.
-       trap("TERM") { consumer.stop }
+    # Stop the consumer when the SIGTERM signal is sent to the process.
+    # It's better to shut down gracefully than to kill the process.
+     trap("TERM") { consumer.stop }
 
-      # This will loop indefinitely, yielding each message in turn.
-        consumer.each_message do |message|
-          @values[message.key]= message.value
-        end
-
-        return @values
+    # This will loop indefinitely, yielding each message in turn.
+      consumer.each_message do |message|
+        @values[message.key]= message.value
       end
+
+      return @values
     end
 
 end
