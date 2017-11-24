1032c13bbc9b2bcbc6843eb5723070c39543b56f
Julián Forero <jforero@mcclatchyservices.com>
2017-11-24 14:43:49 -0500

    Test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,10 +4,6 @@ class TeamsController < ApplicationController
   def index
 
     kafka = Kafka.new(seed_brokers: ["UKRB-INPFTVM-T01:9092"])
-    # Consumers with the same group id will form a Consumer Group together.
-    $consumer = kafka.consumer(group_id: "my-consumer")
-
-    $consumer.subscribe("CTF_countriesbyteam")
     $hash={'2':{}, '3':{}, '4':{} ,'5':{}, '6':{}, '7':{}, '8':{}, '9':{}}
       offset = :earliest
       start_time=Time.now
@@ -41,21 +37,44 @@ class TeamsController < ApplicationController
                             {name: "Los Jimmys", data: $hash[4]},
                             {name: "Los Borbotones", data: $hash[3]},
                             {name: "Chanfle", data: $hash[2]}
-                        ]
-      #$consumer.each_message do |message|
-      #    if !message.nil?
-      #      $recent_messages << [message]
-      #      puts message.value
-      #    end
-      #    $recent_messages.shift if $recent_messages.length > 10
-      #    puts "consumer received message! local message count: #{$recent_messages.size} offset=#{message.offset}"
-      #    $consumer.stop
-      #end
+      ]
+    start_time=Time.now
+
+    loop do
+      messages = kafka.fetch_messages(topic: "CTF_pointsbyteam", partition: 0, offset: offset, max_wait_time: 2)
+      break if Time.now > start_time + 5
+      messages.each do |message|
+        if !message.nil?
+          puts JSON.parse(message.value)['id']
+          puts JSON.parse(message.value)['name']
+          puts JSON.parse(message.value)['ts']
+          puts JSON.parse(message.value)['points']
+          if $hash[(JSON.parse(message.value)['id'])].nil?
+            $hash[(JSON.parse(message.value)['id'])]=Hash.new()
+          end
+          $hash[(JSON.parse(message.value)['id'])][JSON.parse(message.value)['ts']]=JSON.parse(message.value)['points']
+          puts $hash[(JSON.parse(message.value)['id'])]
+          # $recent_messages << [message.value]
+        end
+      end
+      break
+    end
+
+    $recent_messages1=[
+        {name: "ItWorksOnMyContainer", data: $hash[9]},
+        {name: "Access Denied", data: $hash[8]},
+        {name: "Demogorgons", data: $hash[7]},
+        {name: "avadne", data: $hash[6]},
+        {name: "Bits Please", data: $hash[5]},
+        {name: "Los Jimmys", data: $hash[4]},
+        {name: "Los Borbotones", data: $hash[3]},
+        {name: "Chanfle", data: $hash[2]}
+    ]
       rescue Exception => e
         puts 'CONSUMER ERROR'
         puts "#{e}\n#{e.cause}"
         exit(1)
-      return $recent_messages
+      return $recent_messages, $recent_messages1
   end
 
   def indexold
