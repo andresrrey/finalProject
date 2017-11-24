7afb1c6f0764860d652873d0fbfdd965219ca812
Andres Romero <andres.romero@endava.com>
2017-11-24 10:14:16 -0500

    Adding topic data to interface

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,18 +2,28 @@ class TeamsController < ApplicationController
   require "kafka"
 
   def index
-    kafka = Kafka.new(
-        # At least one of these nodes must be available:
-        seed_brokers: ["UKRB-INPFTVM-T01:9092"],
+    kafka = Kafka.new(seed_brokers: ["kafka1:9092", "kafka2:9092"])
 
-        # Set an optional client id in order to identify the client to Kafka:
-    )
-    @values=Hash.new
-    kafka.each_message(topic: "CTF_countriesbyteam") do |message|
-      @values[message.key]=message.value
+    # Consumers with the same group id will form a Consumer Group together.
+    consumer = kafka.consumer(group_id: "my-consumer")
+    # Messages will not be marked as processed automatically. If you shut down the
+    # consumer without calling `#mark_message_as_processed` first, the consumer will
+    # not resume where you left off!
+    consumer.each_message(automatically_mark_as_processed: false) do |message|
+      # Our messages are JSON with a `type` field and other stuff.
+      consumer.subscribe("CTF_countriesbyteam")
 
+      # Stop the consumer when the SIGTERM signal is sent to the process.
+      # It's better to shut down gracefully than to kill the process.
+       trap("TERM") { consumer.stop }
+
+      # This will loop indefinitely, yielding each message in turn.
+        consumer.each_message do |message|
+          @values[message.key]= message.value
+        end
+
+        return @values
+      end
     end
-    return @values
-  end
 
 end
