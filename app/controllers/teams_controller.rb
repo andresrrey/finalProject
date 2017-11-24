19a5a3cdeb2f30a318c3ca3e4727c42c3b83e5d2
Julián Forero <jforero@mcclatchyservices.com>
2017-11-24 13:03:22 -0500

    Test

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -11,7 +11,7 @@ class TeamsController < ApplicationController
       $consumer.subscribe("CTF_countriesbyteam")
       begin
         $consumer.each_message do |message|
-          $recent_messages << [message, {received_at: message.ts}]
+          $recent_messages << [message]
           $recent_messages.shift if $recent_messages.length > 10
           puts "consumer received message! local message count: #{$recent_messages.size} offset=#{message.offset}"
         end
